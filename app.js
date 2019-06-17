434555fdc7eac9927022841ad4820e3441b8f3dc
Blaud <blaudlewis@hotmail.fr>
2019-06-17 16:44:55 +0200

    Beaucoup trop de changement !

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// LES MODULES ====================================
 const express = require("express");
 const exphbs = require('express-handlebars');
 const mongoose = require("mongoose");
@@ -8,44 +9,16 @@ const MongoStore = require("connect-mongo");
 const connectFlash = require("connect-flash");
 const {stripTags} = require ('./helpers/hbs');
 
+// EXPRESS ==============================================
 const app = express();
-var PORT = process.env.PORT || 3050;
-const http = require('http');
-const server = http.Server(app);
 
-
-// Controller
-//Article
-const articleAddcontroller = require("./controllers/articleAdd")
-const articleSinglecontroller = require("./controllers/articleSingle")
-const articlePostcontroller = require("./controllers/articlePost")
-const homepage = require("./controllers/homepage")
-const articleEdit = require ("./controllers/articleEdit")
-
-
-
-// User
-const userCreate = require("./controllers/userCreate")
-const userRegister = require("./controllers/userRegister")
-const userLogin = require("./controllers/userLogin")
-const userLoginAuth = require("./controllers/userLoginAuth")
-const userLogout = require ('./controllers/userLogout')
-
-// Galerie
-const galerieAddcontroller = require ("./controllers/galerieAdd")
-const galerieSinglecontroller = require ("./controllers/galerieSingle")
-const galeriePostcontroller = require ("./controllers/galeriePost")
-const galerieEdit = require ("./controllers/galerieEdit")
-
-
-
-
-//MongoDB
+//MongoDB ===============================================
 // const db = require('./config/keys').MongoURI
 // mongoose
 //     .connect(db, {useNewUrlParser: true , useCreateIndex: true})
 //     .then(()=> console.log('Connecter à MongoDB Cloud'))
 //     .catch(err => console.log(err));
+// MONGOOSE CONNECT =====================================
 const mongoStore = MongoStore(expressSession)
 mongoose.connect('mongodb+srv://blog:QV1cpLkwrEkO9eZH@blog-afur9.mongodb.net/test?retryWrites=true', {useNewUrlParser: true , useCreateIndex: true})
 
@@ -60,22 +33,18 @@ app.use(expressSession({
         mongooseConnection: mongoose.connection
     })
 }))
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(fileupload());
 
+// MIDDLEWARE AUTH ======================================
 const auth = require("./middleware/auth")
 const redirectAuthSuccess = require("./middleware/redirectAuthSucess")
 
+// HANDLEBARS =========================================
 var Handlebars = require("handlebars");
 var MomentHandler = require("handlebars.moment");
 MomentHandler.registerHelpers(Handlebars);
 app.use(express.static('public'));
 
-//route
 app.engine('handlebars', exphbs({
     helpers: {
         stripTags : stripTags
@@ -89,30 +58,56 @@ app.use("*", (req, res, next) => {
     next()
 })
 
+// *Controller
+const homepage = require("./controllers/homepage")
+//* ARTICLE =============================================
+const articleAddcontroller = require("./controllers/articleAdd")
+const articleSinglecontroller = require("./controllers/articleSingle")
+const articlePostcontroller = require("./controllers/articlePost")
+const articleEdit = require ("./controllers/articleEdit")
 
-//middleware
+//* USER ===============================================
+const userCreate = require("./controllers/userCreate")
+const userRegister = require("./controllers/userRegister")
+const userLogin = require("./controllers/userLogin")
+const userLoginAuth = require("./controllers/userLoginAuth")
+const userLogout = require ('./controllers/userLogout')
+
+// Galerie
+const galerieAddcontroller = require ("./controllers/galerieAdd")
+const galerieSinglecontroller = require ("./controllers/galerieSingle")
+const galeriePostcontroller = require ("./controllers/galeriePost")
+const galerieEdit = require ("./controllers/galerieEdit")
+
+// BODYPARSER ===========================================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// ROUTES ===============================================
+app.get("/", homepage);
+
+// MIDDLEWARE ===========================================
 const articleValidPost = require("./middleware/articleValidPost")
 app.use("/articles/post", articleValidPost);
 app.use("./articles/add", auth);
 
 
-app.get("/", homepage);
 
-
-//articles
+// ARTICLES =============================================
 app.get("/articles/add", auth, articleAddcontroller)
 app.get("/articles/:id", articleSinglecontroller)
 app.post("/articles/post", auth, articleValidPost, articlePostcontroller)
 app.get("/article-edit/:id", articleEdit)
 
-// Galerie
-app.get("/galerie/add",galerieAddcontroller)
+// GALERIES =============================================
+app.get("/galerie/add", auth, galerieAddcontroller)
 app.get("/galeries/:id", galerieSinglecontroller)
-app.post("/galeries/post", articleValidPost ,galeriePostcontroller)
+app.post("/galeries/post", auth ,articleValidPost ,galeriePostcontroller)
 app.get("/galerie-edit/:id", galerieEdit)
 
-//EDIT ARTICLES+++++++++++++++++++++++++++++++++++++++
-
+//EDIT ARTICLES =========================================
 app.post('/article/edit/:id', function(req,res){
     const Article = require('./database/models/Article');
     const path = require('path')
@@ -132,17 +127,15 @@ app.post('/article/edit/:id', function(req,res){
             })
     });
     
-    //DELETE ARTICLES+++++++++++++++++++++++++++++
-
+//DELETE ARTICLES ========================================
     app.get('/article-delete/:id', function (req, res) {
            const Article = require('./database/models/Article')
            Article.findByIdAndRemove({_id: req.params.id}).then(function(article){})
     res.redirect('/');
 });
 
-// Galerie
-//EDIT GALERIE+++++++++++++++++++++++++++++++++++++++
-
+// GALERIE ==============================================
+//EDIT GALERIE ==========================================
 app.post('/galerie/edit/:id', function(req,res){
     const Galerie = require('./database/models/Galerie');
     const path = require('path')
@@ -161,21 +154,15 @@ app.post('/galerie/edit/:id', function(req,res){
             })
     });
     
-    //DELETE GALERIE+++++++++++++++++++++++++++++
-
+//DELETE GALERIE =========================================
     app.get('/galerie-delete/:id', function (req, res) {
            const Galerie = require('./database/models/Galerie')
            Galerie.findByIdAndRemove({_id: req.params.id}).then(function(galerie){})
     res.redirect('/');
 });
 
-//contact
-app.get("/contact", function (req, res) {
-    res.render("contact")
-})
 
-
-//users
+// USERS ================================================
 app.get("/user/create", redirectAuthSuccess, userCreate)
 app.post("/user/register", redirectAuthSuccess, userRegister)
 app.get("/user/login", redirectAuthSuccess, userLogin)
@@ -183,13 +170,18 @@ app.post("/user/loginAuth", redirectAuthSuccess, userLoginAuth)
 app.get ('/user/logout', userLogout)
 
 
+// CONTACT ==============================================
+app.get("/contact", function (req, res) {
+    res.render("contact")
+})
+
+// ERROR ================================================
 app.use ((req, res) => {
     res.render('error404')
 })
 
 
-
-
-server.listen(PORT, function () {
-    console.log("le serveur tourne sur le port", PORT);
-  });
+app.listen(3050, function(){
+    // console.log("le serveur tourne sur le port 3000");
+    console.log(`http://localhost:3050`);
+})
--- controllers/userRegister.js
+++ controllers/userRegister.js
@@ -6,12 +6,8 @@ module.exports = (req, res) => {
             if (error) {
                 const registerError = Object.keys(error.errors).map(key => error.errors[key].message);
 
-
-
                 req.flash("registerError", registerError)
                 req.flash("data", req.body)
-
-
 
                 return res.redirect("/user/create")
             }
