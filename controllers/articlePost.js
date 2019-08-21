71289212965bffabc170bbf813b46375563cb60f
Blaud <blaudlewis@hotmail.fr>
2019-08-21 14:27:30 +0200

    test

--- controllers/articlePost.js
+++ controllers/articlePost.js
@@ -4,9 +4,7 @@ const Post = require("../database/models/Article")
 
 module.exports = (req, res) => {
 
-    const {
-        image
-    } = req.files
+    const {image} = req.files
     const uploadFile = path.resolve(__dirname, "..", 'public/articles', image.name);
 
     image.mv(uploadFile, (error) => {
--- controllers/galeriePost.js
+++ controllers/galeriePost.js
@@ -3,7 +3,7 @@ const Post = require("../database/models/Galerie")
 
 module.exports = (req, res) => {
     
-    const { image } = req.files
+    const {image} = req.files
     const uploadFile = path.resolve(__dirname, "..", 'public/galeries', image.name);
  
   image.mv(uploadFile,(error) =>{
