f1e0f8c7f55b66a0f425e63f8b5b38021c961a74
GuYzMoGwAi <blaudlewis@hotmail.fr>
2019-09-03 22:51:20 +0200

    quelque meta en plus

---
+++ test/Spec.js
@@ -0,0 +1,27 @@
+const mocha = require ('mocha');
+
+const assert = require('assert');
+
+const Articles = require('../database/models/Article');
+
+describe('saving to mongodb', function(){
+
+it('save records to a database', function(){
+
+    let Art = new Articles({
+
+        title:'Cévenn’ink Tattoo '
+
+    })
+
+    Art.save().then(function(){
+
+        assert(char.isNew === false);
+
+        done();
+
+    });
+
+});
+
+});
